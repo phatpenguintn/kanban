222e92e82129dab85aaf45ab2a320f2ddc27aec2
Timothy Naylor <tnaylor@searchtechnologies.com>
2017-02-17 18:16:09 -0700

    Add newTask form and tie to data source

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,5 +12,16 @@ export class AppComponent {
         new Task(0,"Do Some stuff","description","in Progress"),
         new Task(1,"Buy a monkey","description","in Progress")
     ]
+
+    private formTask = new Task(0,null,null,"Not Started");
+
+    addTask() {
+        let newTask = new Task (this.formTask.id, this.formTask.title, this.formTask.description, this.formTask.status);
+        this.taskArray.push(newTask);
+        this.formTask.id = parseInt(this.formTask.id) + 1;
+        this.formTask.title = null;
+        this.formTask.description = null;
+        this.formTask.status = "Not Started";
+    }
  
 }
